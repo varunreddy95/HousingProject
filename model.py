3df8b9ec995765f5b49f78aa5cd1ab0b76b169f4
SAI VARUN REDDY <varunreddy95@users.noreply.github.com>
2020-09-29 21:31:00 +0530

    dataset split done. Next is EDA

--- model.py
+++ model.py
@@ -17,4 +17,25 @@ import matplotlib.pyplot as plt
 housing.hist(bins = 50, figsize =(20,15))
 plt.show()
 
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(housing, test_size= 0.2, random_state= 1)
 
+
+import numpy as np
+
+'''
+When we closely observe at some of the attributes, we can see that the ones such as median_income which can have a high
+impact on target variable (median_house_value) is skewed to the right, so when we split the data into train and test with
+the simple random sampling, which might result in considerable sample bias. Hence we shall try stratified sampling in splitting
+the data based on median_income
+'''
+
+housing["income_cat"] = pd.cut(housing["median_income"], bins= [0.0, 1.5, 3.0, 4.5, 6, np.inf], labels = [1,2,3,4,5])
+from sklearn.model_selection import StratifiedShuffleSplit
+split = StratifiedShuffleSplit(n_splits= 1, test_size= 0.2, random_state= 42)
+housing["income_cat"].hist()
+plt.show()
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
