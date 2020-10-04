13013fefea83d00dad34374c44dfa5ea5f7f0c54
SAI VARUN REDDY <varunreddy95@users.noreply.github.com>
2020-10-05 01:58:20 +0530

    GridSearchCV: Fine Tuning the model

--- model.py
+++ model.py
@@ -255,6 +255,21 @@ forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels, sc
 forest_rmse_scores = np.sqrt(-forest_scores)
 display_scores(forest_rmse_scores)
 
+'''
+Hyperparameter Tuning
+'''
+
+from sklearn.model_selection import GridSearchCV
+param_grid = [
+    {'n_estimators': [3,10,30], 'max_features': [2,4,6,8]},
+    {'bootstrap': [False], 'n_estimators': [3,10], 'max_features': [2,3,4]}
+]
+
+forest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forest_reg, param_grid, cv = 5, scoring= 'neg_mean_squared_error', return_train_score= True)
+grid_search.fit(housing_prepared, housing_labels)
+
+
 
 
 
